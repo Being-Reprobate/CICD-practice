81b2113db5f64afcc25063b77462330658190479
Being-Reprobate <145685176+Being-Reprobate@users.noreply.github.com>
2024-06-03 18:06:10 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull beingreprobate/simple-python-flask-app
+docker pull beingreprobate/simple-python-flask-app:latest
 
 # Run the Docker image as a container
-docker run -d -p 8000:8000 beingreprobate/simple-python-flask-app
+docker run -d -p 8000:8000 beingreprobate/simple-python-flask-app:latest
